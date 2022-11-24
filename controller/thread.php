bc627046ba226b2ccfa410c8e2e5bf90682af307
Jari-Matti <e1700833@edu.vamk.fi>
2022-11-24 22:54:59 +0200

    Added GET all threads endpoint

--- controller/thread.php
+++ controller/thread.php
@@ -8,8 +8,6 @@ require_once('../model/ResponseModel.php');
 try {
     $writeDB = DB::connectWriteDB();
     $readDB = DB::connectReadDB();
-
-//throw 500 if failed
 } catch (PDOException $e) {
     //0 = php error logfile
     error_log("Connection error - ".$e, 0);
@@ -21,53 +19,97 @@ try {
     exit();
 }
 
-if(array_key_exists("id", $_GET)) {
-
-    $Id = $_GET['id'];
-    //if id is invalid, return 400
-    if ($Id == '' || !is_numeric($Id)){
-        $response = new ResponseModel();
-        $response->setHttpStatusCode(400);
-        $response->setSuccess(false);
-        $response->addMessage("Invalid thread id");
-        $response->send();
-        exit();
-    }
-}
-
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    #region GET single thread by ID and all of its messages 
 
-    try {
-        //query to get thread by id,
-        $query = $readDB->prepare('SELECT t.id AS threadId, u.username AS threadUsername, u.id AS threadUserId, t.content AS threadContent, DATE_FORMAT(t.createdAt, "%d.%m.%Y %H:%i") AS threadCreatedAt, p.id AS postId , p.content AS postContent, DATE_FORMAT(p.createdAt, "%d.%m.%Y %H:%i") AS postCreatedAt, pu.id AS postUserId, pu.username AS postUsername FROM thread t INNER JOIN post p ON t.id = p.threadId INNER JOIN user u ON t.userId = u.id INNER JOIN user pu ON p.userId = pu.id WHERE t.id = :threadid');
-        $query->bindParam(':threadid', $Id, PDO::PARAM_INT);
-        $query->execute();
-        $rowCount = $query->rowCount();
+    if(array_key_exists("id", $_GET)) {
 
-        //if query is empty, return 404
-        if($rowCount === 0) {
+        $Id = $_GET['id'];
+        //if id is invalid, return 400
+        if ($Id == '' || !is_numeric($Id)){
             $response = new ResponseModel();
-            $response->setHttpStatusCode(404);
+            $response->setHttpStatusCode(400);
             $response->setSuccess(false);
-            $response->addMessage("Thread not found");
+            $response->addMessage("Invalid thread id");
             $response->send();
             exit();
         }
-        
-        $i=true;
-        //insert query to thread and post models and return 200
-        while($row = $query ->fetch(PDO::FETCH_ASSOC)) {
-
-            if($i==true){
-                $thread = new Thread($row['threadId'], $row['threadUserId'], $row['threadUsername'], $row['threadContent'], $row['threadCreatedAt']);
-                $i=false;
+        try {
+            //query to get thread by id,
+            $query = $readDB->prepare('SELECT t.id AS threadId, u.username AS threadUsername, u.id AS threadUserId, t.content AS threadContent, DATE_FORMAT(t.createdAt, "%d.%m.%Y %H:%i") AS threadCreatedAt, p.id AS postId , p.content AS postContent, DATE_FORMAT(p.createdAt, "%d.%m.%Y %H:%i") AS postCreatedAt, pu.id AS postUserId, pu.username AS postUsername FROM thread t INNER JOIN post p ON t.id = p.threadId INNER JOIN user u ON t.userId = u.id INNER JOIN user pu ON p.userId = pu.id WHERE t.id = :threadid');
+            $query->bindParam(':threadid', $Id, PDO::PARAM_INT);
+            $query->execute();
+            $rowCount = $query->rowCount();
+            //if query is empty, return 404
+            if($rowCount === 0) {
+                $response = new ResponseModel();
+                $response->setHttpStatusCode(404);
+                $response->setSuccess(false);
+                $response->addMessage("Thread not found");
+                $response->send();
+                exit();
             }
-            $post = new Post($row['postId'], $row['postCreatedAt'], $row['postUserId'], $row['postUsername'], $row['postContent']);
-            $thread->addPost($post->returnPostAsArray());
+            
+            
+            //insert query to thread and post models and return 200
+            $i=true;
+            while($row = $query ->fetch(PDO::FETCH_ASSOC)) {
+                if($i==true){
+                    $thread = new Thread($row['threadId'], $row['threadUserId'], $row['threadUsername'], $row['threadContent'], $row['threadCreatedAt']);
+                    $i=false;
+                }
+                $post = new Post($row['postId'], $row['postCreatedAt'], $row['postUserId'], $row['postUsername'], $row['postContent']);
+                $thread->addPost($post->returnPostAsArray());
+            }
+                $threads[] = $thread->returnThreadAsArray();
+                $returnArray = array();
+                $returnArray['threads'] = $threads;
+
+                $response = new ResponseModel();
+                $response->setHttpStatusCode(200);
+                $response->setSuccess(true);
+                $response->toCache(true);
+                $response->setData($returnArray);
+                $response->send();
+                exit();
+                
+            }
+        //return 500 if problem with php
+        catch (ThreadException $e){
+            $response = new ResponseModel();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage($e->getMessage());
+            $response->send();
+            exit();
         }
-            $threads[] = $thread->returnThreadAsArray();
+        //return 500 if problem database
+        catch (PDOException $e) {
+            error_log("Database query error - ".$e, 0);
+            $response = new ResponseModel();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage("Failed to get the thread");
+            $response->send();
+            exit();
+        }
+    #endregion
+
+    #region GET all threads
+    } elseif(empty($_GET)) {
+
+        try {
+            $query = $readDB->prepare('SELECT t.id AS threadId, u.username AS threadUsername, u.id AS threadUserId, t.content AS threadContent, DATE_FORMAT(t.createdAt, "%d.%m.%Y %H:%i") AS threadCreatedAt FROM thread t INNER JOIN user u ON t.userId = u.id');
+            $query->execute();
+
+            $threads = array();
+            while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+
+                $thread = new Thread($row['threadId'], $row['threadUserId'], $row['threadUsername'], $row['threadContent'], $row['threadCreatedAt']);
+                $threads[] = $thread->returnThreadAsArray();
+            }
+                
             $returnArray = array();
-            $returnArray['rows_returned'] = $rowCount;
             $returnArray['threads'] = $threads;
 
             $response = new ResponseModel();
@@ -77,34 +119,73 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             $response->setData($returnArray);
             $response->send();
             exit();
-            
+
+        } catch(ThreadException $e) {
+            $response = new ResponseModel();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage($e->getMessage());
+            $response->send();
+            exit();
         }
-    //return 500 if problem with php
-    catch (ThreadException $e){
+        catch(PDOException $e) {
+          error_log("Database Error: ".$e, 0);
+          $response = new ResponseModel();
+          $response->setHttpStatusCode(500);
+          $response->setSuccess(false);
+          $response->addMessage("Failed to get threads");
+          $response->send();
+          exit();
+        }
+
+    } else {
         $response = new ResponseModel();
-        $response->setHttpStatusCode(500);
+        $response->setHttpStatusCode(404);
         $response->setSuccess(false);
-        $response->addMessage($e->getMessage());
+        $response->addMessage("Page not found");
         $response->send();
         exit();
     }
-    //return 500 if problem database
-    catch (PDOException $e) {
-        error_log("Database query error - ".$e, 0);
+    #endregion
+    
+} elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+
+    try {
+        $query = $writeDB->prepare('DELETE FROM thread WHERE id = :threadid');
+        $query->bindParam(':threadid', $Id, PDO::PARAM_INT);
+        $query->execute();
+
+        $rowCount = $query->rowCount();
+        if($rowCount === 0) {
+            $response = new ResponseModel();
+            $response->setHttpStatusCode(404);
+            $response->setSuccess(false);
+            $response->addMessage("Thread not found");
+            $response->send();
+            exit();
+        }
+
+        $response = new ResponseModel();
+        $response->setHttpStatusCode(200);
+        $response->setSuccess(true);
+        $response->addMessage("Thread deleted");
+        $response->send();
+        exit();
+
+    } catch (PDOException $e) {
         $response = new ResponseModel();
         $response->setHttpStatusCode(500);
         $response->setSuccess(false);
-        $response->addMessage("Failed to get the thread");
+        $response->addMessage("Thread deleted");
         $response->send();
         exit();
     }
 
-} elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-    exit();
 } elseif ($_SERVER['REQUEST_METHOD'] === 'PATCH') {
+
     exit();
-}
-else {
+
+} else {
     $response = new ResponseModel();
     $response->setHttpStatusCode(405);
     $response->setSuccess(false);
